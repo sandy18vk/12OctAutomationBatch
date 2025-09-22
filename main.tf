5d3afdad3246987c4c90fd79e4bea2ee4e5fd6fb
Faisal Ansari <faisalansari4698@gmail.com>
2025-09-22 15:44:37 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = buoyant-keel-455805-r7
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_storage_bucket" "demo" {
+  name          = var.bucket_name
+  location      = var.region
+  force_destroy = true
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
